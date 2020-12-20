2a029bbcf23e72e291e53d653d554bd3499b1d18
SableSmile <32065905+SableSmile@users.noreply.github.com>
2020-12-20 16:55:30 +0800

    1220 Script

--- 2D_HollowKnight/Assets/Scripts/Tip.cs
+++ 2D_HollowKnight/Assets/Scripts/Tip.cs
@@ -1,18 +1,28 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Tip : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("速度"),Range(1,20)]
+    public int speed;
+    public GameObject character;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [Header("距離")]
+    [Range(5.5f,20.5f)]
+    public float distancef;    
+    public string log_text="提示文字";
+    public bool active=true;
+    public Color rgb=new Color(0.3f,0.5f,0.8f);
+    public Color color2=Color.gray;
+    public Color rgba=new Color(0.8f,0.3f,0.8f,0.4f);
+
+    public Vector2 pos2=new Vector2(12,-15);
+    public Vector3 pos3=new Vector3(12,15,16);
+    public Sprite pic;
+    public AudioClip sound;
+    public Camera came;
+    public Transform tran;
+
+
+
+ 
 }
---
+++ 2D_HollowKnight/Assets/Scripts/player.cs
@@ -0,0 +1,44 @@
+﻿
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    [Header("移動速度"),Range(0f,1000f)]
+    public float MoveSpeed;
+    
+    [Header("跳躍高度"),Range(0,3000)]
+    public int JumpHeight;
+
+    [Header("是否在地板上")] //預設為否
+    public bool OnGround;
+
+    [Header("子彈")] //物件 子彈
+    public GameObject bullet;
+
+    [Header("子彈生成點")] //物件位置
+    public Transform bullet_pos;
+
+    [Header("子彈速度"),Range(0,5000)]
+    public int bullet_speed;   
+
+    [Header("開槍音效")] //槍聲
+    public AudioClip shoot_sound;   
+
+    [Header("血量"),Range(0,200)]
+    public int HP;    
+
+    private AudioSource audioSource;
+    private Rigidbody2D rb;
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
