6facd04fc4b9156ec848950120a75bab2a50f05a
SableSmile <32065905+SableSmile@users.noreply.github.com>
2021-01-17 17:04:44 +0800

    0117敵人資料設定

---
+++ 2D_HollowKnight/Assets/Scripts/Enemy.cs
@@ -0,0 +1,39 @@
+﻿
+using UnityEngine;
+//第一次套用腳本
+[RequireComponent(typeof(AudioSource),typeof(Rigidbody2D),typeof(CapsuleCollider2D))]
+
+public class Enemy : MonoBehaviour
+{
+    [Header("移動速度"), Range(0f, 1000)]
+    public float enemy_MoveSpeed;
+    [Header("血量"), Range(0, 5000)]
+    public int enemy_HP;
+    [Header("攻擊範圍"), Range(0, 100)]
+    public int enemy_AttackRange;
+    [Header("攻擊力"), Range(0, 1000)]
+    public int enemy_Attack;
+    
+    private AudioSource au;
+    private Rigidbody2D rig;
+    private Animator an;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        au=GetComponent<AudioSource>();
+        rig=GetComponent<Rigidbody2D>();
+        an=GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void hurt(int damage){
+        enemy_HP-=damage;
+        an.SetTrigger("hurt");
+    }
+}
---
+++ 2D_HollowKnight/Assets/Scripts/bullet.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    /// <summary>
+    /// 子彈攻擊力
+    /// </summary>
+    public int attack;
+    
+    /// <summary>
+    /// 碰撞事件:2個物件都沒有勾選is trigger才可觸發
+    /// </summary>
+    /// <param name="col"></param>
+    private void OnCollisionEnter2D(Collision2D col) {
+        if(col.gameObject.GetComponent<Enemy>()){
+            col.gameObject.GetComponent<Enemy>().hurt(attack);
+        }
+    }
+}
